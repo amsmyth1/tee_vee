6c3e383f3331a39e1b34dff77fba897ed01268d7
Alexa Morales Smyth <alexamsmyth@gmail.com>
2020-12-15 11:37:27 -0500

    Create total_salary and highest_paid_actor methods

--- lib/show.rb
+++ lib/show.rb
@@ -8,4 +8,25 @@ class Show
     @creator = creator
     @characters = characters
   end
-end 
+
+  def total_salary
+    #calculate total salary of characters
+    total_salary = 0
+    @characters.each do |character|
+      total_salary += character.salary
+    end
+    total_salary
+  end
+
+  def highest_paid_actor
+    highest = @characters[0].salary
+    @characters.each do |character|
+      if character.salary > highest
+        highest = character
+      end
+    end
+    highest.actor
+  end
+
+  
+end
--- test/show_test.rb
+++ test/show_test.rb
@@ -22,4 +22,46 @@ class ShowTest < MiniTest::Test
     assert_equal "Glen Larson", show.creator
     assert_equal [character_1, character_2], show.characters
   end
+
+  def test_show_total_salary
+    character_1 = mock
+    character_2 = mock
+    character_1.stubs(:salary).returns(1)
+    character_2.stubs(:salary).returns(2)
+    show = Show.new("Knight Rider", "Glen Larson", [character_1, character_2])
+
+    assert_equal 3, show.total_salary
+  end
+
+  def test_highest_paid_actor
+    character_1 = mock
+    character_2 = mock
+    character_1.stubs(:salary).returns(1)
+    character_2.stubs(:salary).returns(2)
+    character_1.stubs(:actor).returns("William Daniaels")
+    character_2.stubs(:actor).returns("David Hasselhoff")
+    # character_1 = Character.new({
+    #                     name: "KITT",
+    #                     actor: "William Daniels",
+    #                     salary: 1_000_000})
+    # character_2 = Character.new({
+    #                     name: "Michael Knight",
+    #                     actor: "David Hasselhoff",
+    #                     salary: 1_600_000})
+    show = Show.new("Knight Rider", "Glen Larson", [character_1, character_2])
+
+    assert_equal "David Hasselhoff", show.highest_paid_actor
+  end
+
+  def test_actors
+    character_1 = mock
+    character_2 = mock
+    character_1.stubs(:salary).returns(1)
+    character_2.stubs(:salary).returns(2)
+    character_1.stubs(:actor).returns("William Daniaels")
+    character_2.stubs(:actor).returns("David Hasselhoff")
+    show = Show.new("Knight Rider", "Glen Larson", [character_1, character_2])
+
+    assert_equal ["William Daniaels", "David Hasselhoff"], show.actors 
+  end
 end
